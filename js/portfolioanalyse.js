0cb4ee225a31c8fc24539137742e6d75ad0792df
Florian Dagner <dagner@srs-n3.schulen-nuernberg.de>
2024-05-03 14:06:49 +0200

    some changes bwr3 portfolio

--- js/portfolioanalyse.js
+++ js/portfolioanalyse.js
@@ -1,3 +1,5 @@
+generatePortfolio();
+
 function randomizePortfolios() {
   const portfolios = [
     document.getElementById('portfolioA').value,
@@ -59,6 +61,64 @@ function randomizePortfolios() {
     svg.appendChild(text);
   });
 }
+
+function generatePortfolio() {
+
+  const svgWidth = 550;
+  const svgHeight = 380;
+
+  const portfolios = [
+    {
+      name: document.getElementById('portfolioA').value,
+      x: 50 + parseFloat(svgWidth) * parseFloat(document.getElementById('portfolioAx').value) / 200,
+      y: svgHeight - (parseFloat(svgHeight) * parseFloat(document.getElementById('portfolioAy').value) / 40),
+    },
+    {
+      name: document.getElementById('portfolioB').value,
+      x: 50 + parseFloat(svgWidth) * parseFloat(document.getElementById('portfolioBx').value) / 200,
+      y: svgHeight - (parseFloat(svgHeight) * parseFloat(document.getElementById('portfolioBy').value) / 40),
+    },
+    {
+      name: document.getElementById('portfolioC').value,
+      x: 50 + parseFloat(svgWidth) * parseFloat(document.getElementById('portfolioCx').value) / 200,
+      y: svgHeight - (parseFloat(svgHeight) * parseFloat(document.getElementById('portfolioCy').value) / 40),
+    },
+    {
+      name: document.getElementById('portfolioD').value,
+      x: 50 + parseFloat(svgWidth) * parseFloat(document.getElementById('portfolioDx').value) / 200,
+      y: svgHeight - (parseFloat(svgHeight) * parseFloat(document.getElementById('portfolioDy').value) / 40),
+    }
+  ];
+
+  const svg = document.getElementById('portfolioSVG');
+
+  const existingItems = svg.querySelectorAll('.item');
+  existingItems.forEach(item => item.remove());
+
+  portfolios.forEach(portfolio => {
+    const circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+    circle.setAttribute('cx', portfolio.x);
+    circle.setAttribute('cy', portfolio.y);
+    circle.setAttribute('r', 10);
+    circle.setAttribute('fill', getRandomColor());
+    circle.classList.add('item');
+    circle.classList.add('circle');
+    svg.appendChild(circle);
+
+    const text = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+    text.setAttribute('x', portfolio.x);
+    text.setAttribute('y', portfolio.y);
+    text.setAttribute('font-size', 18);
+    text.setAttribute('fill', '#333');
+    text.setAttribute('transform', 'translate(15,5)');
+    text.textContent = portfolio.name;
+    text.classList.add('item');
+    text.classList.add('text');
+    svg.appendChild(text);
+  });
+}
+
+
 
 
 function getRandomColor() {
